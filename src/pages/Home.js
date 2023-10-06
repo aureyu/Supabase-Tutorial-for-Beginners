0988d58b43b96a12dce44ceac01eb210108d4467
Yijun Yu <aureyu@gmail.com>
2023-10-06 03:38:55 +0000

    Changes to be committed: 	modified:   src/pages/Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,31 @@
 import supabase from "../config/supabaseClient"
+import { useEffect, useState } from "react" 
 
 const Home = () => {
-  console.log(supabase)
-  
+  const[fetchError, setFetchError] = useState(null)
+  const[smoothies, setSmoothies] = useState(null)
+
+  useEffect(() => {
+    const fetchSmoothies = async () => {
+      const {data, error} = await supabase
+      .from('smoothies')
+      .select()
+
+      if (error){
+        setFetchError('Could not fetch')
+        setSmoothies(null)
+        console.log(error)
+      }
+      if(data){
+        setSmoothies(data)
+        setFetchError(null)
+      }
+    }
+
+    fetchSmoothies()
+
+  }, [])
+
   return (
     <div className="page home">
       <h2>Home</h2>
